464fb6bfa094680f7621ca2ad9391e74292cc2c9
Gregorio <116957147+Gregorio-Desantadina@users.noreply.github.com>
2024-01-12 23:38:53 -0300

    Add files via upload

--- Projecto1/Projecto1/Program.cs
+++ Projecto1/Projecto1/Program.cs
@@ -10,17 +10,29 @@ namespace Projecto1
             Character pepe = new Character();
             Character enemigo = new Character();
             FireRing Anillo = new FireRing();
+            SpikeArmour armadura = new SpikeArmour();
 
 
-            pepe.attack();
+            Console.WriteLine("Armour test (Action being overwritted to change the target of the damage)");
+            armadura.Equip(enemigo);
+            enemigo.reciveDamage(10, pepe);
+            
 
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Action making 2 attacks at the same time with just one call");
             Anillo.Equip(pepe);
 
-            pepe.attack();
+            pepe.attack(enemigo);
 
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Unequip object and attacking again");
             Anillo.Unequip();
 
-            pepe.attack();
+            pepe.attack(enemigo);
             Console.ReadLine();
 
 
@@ -40,33 +52,69 @@ namespace Projecto1
         {
             if (owner.attack != null)
             {
+                owner.attack -= OwnerAttack; 
                 owner = null;
                 Console.WriteLine("No equipado");
-                //Error que no puedo solucionar
-                //owner.attack -= OwnerAttack; 
+               
+                
             }
         }
-        public void OwnerAttack()
+        public void OwnerAttack(Character target)
         {
             Console.WriteLine("El anillo tambien ataca");
-            
+            target.damageManager(10, owner);
+
+        }
+    }
+
+    public class SpikeArmour
+    {
+        private Character owner = null;
+        public void Equip(Character newOwner)
+        {
+            owner = newOwner;
+            Console.WriteLine("Armadura Equipado");
+            newOwner.reciveDamage += OwnerRecivesDamage;
+            newOwner.reciveDamage -= newOwner.damageManager;
+        }
+        public void Unequip()
+        {
+            if (owner.attack != null)
+            {
+                owner.reciveDamage -= OwnerRecivesDamage;
+                owner.reciveDamage += owner.damageManager;
+                owner = null;
+                Console.WriteLine("No equipado");
+            }
+        }
+        public void OwnerRecivesDamage(int damage, Character target)
+        {
+            Console.WriteLine("La armadura recive el daño y ataca en su lugar");
         }
     }
 
     public class Character
     {
         public string name;
-        public Action attack;
+        public Action<Character> attack;
+        public Action<int, Character> reciveDamage;
 
         public Character()
         {
             name = "pepe";
             attack += Attack;
+            reciveDamage += damageManager;
         }
 
-        public void Attack()
+        public void Attack(Character target)
         {
-            Console.WriteLine("El personaje ataca");  
+            Console.WriteLine($"El personaje ataca a {target.name}");
+            target.damageManager(10, this);
+        }
+
+        public void damageManager(int damage, Character damageDealer)
+        {
+            Console.WriteLine($"Has recivido daño de parte de {name}");
         }
 
        
